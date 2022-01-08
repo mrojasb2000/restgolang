e0e11e792e85471c22c8e90d0cbbc27f2464962f
mavro <mrojasb2000@gmail.com>
2022-01-08 19:01:32 -0300

    Refactor application router

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -25,12 +25,12 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 
 	// We can then use the ByNames() method to get the value of the "id" parameter from
 	// the slice. In our project all movies will have a unique positive integer ID, but
-	// the value returned by ByName() es always a string. So we try to convert it to a
+	// the value returned by ByName() is always a string. So we try to convert it to a
 	// base 10 integer (with a bit size of 64). If the parameter couldn't be converted,
 	// or is less than 1, we know the ID is invalid so we use the http.NotFound()
 	// function to return a 404 Not Found response.
 	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil {
+	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
 	}
