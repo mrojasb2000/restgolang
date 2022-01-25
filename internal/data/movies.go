7ccc55ade2bbed3e26a0ca100b8bc9555e0f7646
mavro <mrojasb2000@gmail.com>
2022-01-25 16:04:36 -0300

    Added struct tags omitempty directive

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -5,11 +5,11 @@ import "time"
 // Annotate the Movie struct tags to control how the keys appear in the
 // JSON-encoded output.
 type Movie struct {
-	ID       int64     `json:"id"`         // Unique integer ID for the movie
-	CreateAt time.Time `json:"created_at"` // Timestamp for when the movie is added to our database
-	Title    string    `json:"title"`      // Movie title
-	Year     int32     `json:"year"`       // Movie release year
-	Runtime  int32     `json:"runtime"`    // Movie runtime (in minutes)
-	Genres   []string  `json:"genres"`     // Slice of genres number for the movie (romance, comedy, etc.)
-	Version  int32     `json:"version"`    // time the movie information is updated
+	ID       int64     `json:"id"`                // Unique integer ID for the movie
+	CreateAt time.Time `json:"-"`                 // Timestamp for when the movie is added to our database
+	Title    string    `json:"title"`             // Movie title
+	Year     int32     `json:"year,omitempty"`    // Movie release year
+	Runtime  int32     `json:"runtime,omitempty"` // Movie runtime (in minutes)
+	Genres   []string  `json:"genres,omitempty"`  // Slice of genres number for the movie (romance, comedy, etc.)
+	Version  int32     `json:"version"`           // time the movie information is updated
 }
